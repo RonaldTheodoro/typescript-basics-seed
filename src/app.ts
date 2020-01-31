2ec990619dd4757c7c1568a632cf0577ab804d42
Ronald Theodoro <ronaldtheodorodigesto@gmail.com>
2020-01-31 10:56:37 -0300

    Arrow functions

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log('Hello TypeScript!');
+const pizzas = [
+    { name: 'Pepperoni', toppings: ['pepperoni'] },
+    { name: 'Cheese', toppings: ['cheese'] },
+];
+
+const mappedPizzas = pizzas.map(pizza => pizza.name.toUpperCase());
+
+console.log(mappedPizzas);
+
+const pizza = {
+    name: 'Blazing inferno',
+    getName: function () {
+        console.log(this.name);
+    }
+}
+
+pizza.getName();
