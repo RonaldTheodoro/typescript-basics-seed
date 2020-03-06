e8ffbba876b11e40405cf81ba6780dc125f51e24
Ronald Theodoro <ronaldtheodorodigesto@gmail.com>
2020-03-06 12:51:06 -0300

    Functions and Optional Arguments

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
-let sumOrder: (price: number, quantity: number) => number;
+let sumOrder: (price: number, quantity?: number) => number;
 
 
-sumOrder = (x, y) => x * y;
+sumOrder = (x, y) => {
+    if (y) {
+        return x * y;
+    }
+    return x;
+};
 
-const sum: number = sumOrder(15, 5);
+const sum: number = sumOrder(15);
 
 console.log(`Total sum: ${sum}`)
