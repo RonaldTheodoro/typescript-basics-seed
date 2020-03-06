a7bf5644232d5026d6c031ba4816d66dfcc064a7
Ronald Theodoro <ronaldtheodorodigesto@gmail.com>
2020-03-06 12:56:39 -0300

    Typed Functions and Default Params

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 let sumOrder: (price: number, quantity?: number) => number;
 
 
-sumOrder = (x, y) => (y) ? x * y : x;
+sumOrder = (x, y = 1) => x * y;
 
 console.log(`Total sum: ${sumOrder(15)}`)
 console.log(`Total sum: ${sumOrder(15, 5)}`)
