af4b55dd1c4239f413114998b7d0173235e3eed7
Ronald Theodoro <ronaldtheodorodigesto@gmail.com>
2020-03-10 10:55:52 -0300

    Type Assertions

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
-type Size = 'small' | 'medium' | 'large'
-type Callback = (size: Size) => void;
+type Pizza = { name: string, toppings: number };
 
-let pizzaSize: Size = 'small';
+const pizza: Pizza = { name: 'Blazing Inferno', toppings: 5 };
 
-const selectSize: Callback = (x) => {
-    pizzaSize = x;
-}; 
+const serialized = JSON.stringify(pizza);
 
-selectSize('small')
+function getNameFromJSON(obj: string) {
+    return (JSON.parse(obj) as Pizza).name;
+}
 
-console.log(pizzaSize)
+
+console.log(getNameFromJSON(serialized));
