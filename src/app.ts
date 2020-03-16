d9c219f7995064c53eab62645ba1203340e57480
Ronald Theodoro <ronaldtheodoro@gmail.com>
2020-03-16 11:47:33 -0300

    Setters and Getters (Accessors)

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,30 @@
-class Pizza {
+class Sizes {
 
-    constructor(readonly name: string, public topping: string[] = []) { }
+    constructor(private sizes: string[]) { }
+
+    set availableSizes(sizes: string[]) {
+        this.sizes = sizes;
+    }
+
+    get availableSizes(): string[] {
+        return this.sizes;
+    }
+
+}
+
+
+const sizes = new Sizes(['small', 'medium']);
+
+console.log(sizes.availableSizes);
+
+sizes.availableSizes = ['medium', 'large'];
+
+console.log(sizes.availableSizes);
+
+class Pizza {
+    public topping: string[] = [];
+
+    constructor(readonly name: string) { }
 
     addTopping(topping: string) {
         this.topping.push(topping);
